e465e97dc38bf1d05ac449adc2b8ffea8da21b44
ddore <ddore@i3s.unice.fr>
2026-04-24 10:55:54 +0200

    fix bash

--- bash.sh
+++
@@ -1,3 +0,0 @@
-python src/misc/fix_spans.py --ann /Users/ddore/Documents/ElecDeb60to20/data/annotations/components/46_2020.ann\
- --txt /Users/ddore/Documents/ElecDeb60to20/data/annotations/txt/46_2020.txt
-
